a7695a5104172731e7415fcff7bc4032b498cbc1
Toacin <toacinp@outlook.com>
2022-10-05 15:57:37 -0700

    added starter code

---
+++ index.js
@@ -0,0 +1,19 @@
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        name: "username",
+        message: "What is your Github Username",
+        
+    }
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
